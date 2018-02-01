a8d10354ad91bf50c1984826449d2da0b4daa9d7
Joseph Klinger <klingerj@seas.upenn.edu>
2018-02-01 01:15:11 -0500

    Preliminary code architecture setup. Need to refactor into files.

--- Trees/Trees/main.cpp
+++ Trees/Trees/main.cpp
@@ -1,5 +1,6 @@
-#include <glad\glad.h>
-#include <GLFW\glfw3.h>
+#include "glad\glad.h"
+#include "GLFW\glfw3.h"
+#include "glm\glm.hpp"
 #include <iostream>
 #include <vector>
 #include <random>
@@ -15,8 +16,31 @@ void processInput(GLFWwindow *window) {
         glfwSetWindowShouldClose(window, true);
 }
 
-int main() {
+// Tree growth code - will need to be refactored TO ANOTHER FILE PLS ALSO CANT TURN OFF CAPS LOCK RN SO
 
+class TreeNode {
+private:
+    glm::vec3 point; // Point in world space
+    float influenceDist; // Radius of sphere of influence
+public:
+    std::vector<TreeNode> children; // make this private later. Also not very memory coalescent
+    TreeNode(const glm::vec3& p, const float& d) : point(p), influenceDist(d) {}
+    ~TreeNode() {}
+    inline bool InfluencesPoint(const glm::vec3& p) const {
+        return glm::length(p - point) < influenceDist;
+    }
+};
+
+class AttractorPoint {
+private:
+    glm::vec3 point; // Point in world space
+    float killDist; // Radius for removal
+public:
+    AttractorPoint(const glm::vec3& p, const float& d) : point(p), killDist(d) {}
+    ~AttractorPoint() {}
+};
+
+int main() {
     // GLFW Window Setup
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -42,7 +66,8 @@ int main() {
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // Test loop vectorization. Note: only seems to compile in Release Mode
+    // Test loop vectorization. Note: using the compiler flags, this stuff only seems to compile in Release Mode
+    // Needed flags: /O2 /Qvec-report:1 (can also use report:2)
     // Source: https://software.intel.com/en-us/articles/a-guide-to-auto-vectorization-with-intel-c-compilers
 
     // first populate the list
@@ -65,9 +90,11 @@ int main() {
 
     // Stores the point positions: currently a list of floats. I need to include glm or eigen
     // Is it faster to initialize a vector of points with # and value and then set the values, or to push_back values onto an empty list
-    const unsigned int numPoints = 5000;
-    unsigned numPointsIncluded = 0;
-    std::vector<float> points = std::vector<float>(); // 1000 points with value 0
+    // Answer to that: https://stackoverflow.com/questions/32199388/what-is-better-reserve-vector-capacity-preallocate-to-size-or-push-back-in-loo
+    // Best options seem to be preallocate or emplace_back with reserve
+    const unsigned int numPoints = 10;
+    unsigned int numPointsIncluded = 0;
+    std::vector<glm::vec3> points = std::vector<glm::vec3>();
 
     // Random number generator. source taken from: http://en.cppreference.com/w/cpp/numeric/random/uniform_real_distribution
 
@@ -76,20 +103,33 @@ int main() {
     std::uniform_real_distribution<float> dis(0.0f, 1.0f);
 
     // Create points
-    for (unsigned int i = 0; i < numPoints * 3; i += 3) {
-        const float x = dis(gen) * 2.0f - 1.0f; // account for aspect ratio
-        const float y = dis(gen) * 2.0f - 1.0f;
-        const float z = dis(gen) * 2.0f - 1.0f;
-        if (std::sqrt(x * x + y * y + z * z) < 0.75f) {
-            points.push_back(x);
-            points.push_back(y);
-            points.push_back(z);
-            numPointsIncluded++;
+    // Unfortunately, we can't really do any memory preallocating because we don't actually know how many points will be included
+    for (unsigned int i = 0; i < numPoints; ++i) {
+        const glm::vec3 p = glm::vec3(dis(gen) * 2.0f - 1.0f, dis(gen) * 2.0f - 1.0f, dis(gen) * 2.0f - 1.0f);
+        if ((p.x * p.x + p.y * p.y + p.z * p.z) < 0.75f) { // sphere sdf, radius of 0.75f
+            points.emplace_back(p);
+            ++numPointsIncluded;
         }
     }
 
-    // Create indices
-    std::vector<unsigned int> indices = std::vector<unsigned int>(numPointsIncluded, 0);
+    // Create the AttractorPoints
+    std::vector<AttractorPoint> attractorPoints = std::vector<AttractorPoint>();
+    attractorPoints.reserve(numPointsIncluded);
+    for (int i = 0; i < numPointsIncluded; ++i) {
+        attractorPoints.emplace_back(AttractorPoint(points[i], 0.15f));
+    }
+
+    // Create the TreeNode(s)
+    const float branchLength = 0.3f;
+    std::vector<TreeNode> treeNodes = std::vector<TreeNode>();
+    treeNodes.emplace_back(TreeNode(glm::vec3(0.0f, 0.0f, 0.0f), 0.5f)); // center of screen
+
+    // Run the tree algorithm
+    //TODO
+
+
+    // Create indices for the attractor points
+    std::vector<unsigned int> indices = std::vector<unsigned int>(numPointsIncluded);
     for (unsigned int i = 0; i < numPointsIncluded; ++i) {
         indices[i] = i;
     }
@@ -100,8 +140,8 @@ int main() {
     unsigned int vertexShader;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
     const char* vertexShaderSource =
-        "#version 450 core\n\nlayout(location = 0) in vec3 vPos;\nlayout(location = 0) out vec3 fPos;\n\nvoid main() {\nfPos = vPos;\ngl_Position = vec4(vPos * vec3(0.75, 1, 1), 1);\n}";
-                                                                                                                                                                  // account for aspect ratio
+        "#version 450 core\n\nlayout(location = 0) in vec3 vPos;\nlayout(location = 0) out vec3 fPos;\n\nvoid main() {\nfPos = vPos;\ngl_Position = vec4(vPos * vec3(600.0 / 800.0, 1, 1), 1);\n}";
+                                                                                                                                               // account for aspect ratio...why am i dividing, not multiplying?
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
     // Shader compilation success check
@@ -116,7 +156,7 @@ int main() {
 
     unsigned int fragmentShader;
     fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    const char* fragmentShaderSource =  "#version 450 core\n\nlayout(location = 0) in vec3 fPos;\nout vec4 FragColor;\n\nvoid main() {\nFragColor = vec4(abs(fPos), 1);\n}";
+    const char* fragmentShaderSource =  "#version 450 core\n\nlayout(location = 0) in vec3 fPos;\nout vec4 FragColor;\n\nvoid main() {\nFragColor = vec4(vec3(1)/*abs(fPos)*/, 1);\n}";
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
     // Shader compilation success check
@@ -141,25 +181,27 @@ int main() {
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
     
-    // VAO Binding
+    /// Array/Buffer Objects
     unsigned int VAO;
+    unsigned int VBO;
+    unsigned int EBO;
     glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    // VAO Binding
     glBindVertexArray(VAO);
 
     // VBO Binding
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 3 * numPoints, points.data(), GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3) * numPointsIncluded, points.data(), GL_STATIC_DRAW);
 
     // EBO Binding
-    unsigned int EBO;
-    glGenBuffers(1, &EBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indices.size(), indices.data(), GL_STATIC_DRAW);
 
     // Attribute linking
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), (void*)0);
     glEnableVertexAttribArray(0);
 
     glBindVertexArray(0); // not really sure what this does
